fc9f4c6c5f941e5b0ec7b7b95309ac74a95432bb
Dan Olaru <dan.olaru@reisystems.com>
2016-11-08 20:39:23 -0500

    re-work wordpress playbook

--- ansible/get-vault-password.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import yaml
-import os.path
-
-with open (os.path.dirname(__file__) + "/../secrets.yml", "r") as myfile:
-    data = yaml.load(myfile)
-
-print data.get('ansible_vault_password')
