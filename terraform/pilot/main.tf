0af0a99448609add4bc4d3a297df061174b0efa9
Neil Hunt <Neil.Hunt@aquilent.com>
2016-11-25 13:47:57 -0500

    adding security groups
    GSA/catalog-deploy#49

--- terraform/pilot/main.tf
+++ terraform/pilot/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "datagov_jump" {
     ami = "${lookup(var.ubuntu_amis, ${var.region}-hardened)}"
     instance_type = "t2.micro"
     key_name = "${var.key_name}"
-#    vpc_security_group_ids = ["sg-ba1b05c1"]
+    vpc_security_group_ids = ["sg-ba1b05c1"]
     
     subnet_id = "subnet-29c04203"
     root_block_device = {
