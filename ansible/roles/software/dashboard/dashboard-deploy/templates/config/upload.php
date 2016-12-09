5351bd850ce1df4b163ec16939b011e391a3d33f
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-12-09 11:08:14 -0500

    pre_approved_admins for dashboard, docker changes for dashboard

--- ansible/roles/software/dashboard/dashboard-deploy/templates/config/upload.php
+++ ansible/roles/software/dashboard/dashboard-deploy/templates/config/upload.php
@@ -6,6 +6,6 @@
 |--------------------------------------------------------------------------
 */
 
-$config['upload_path'] = '{{ project_source_path }}/shared/uploads/'; // absolute path
+$config['upload_path'] = '{{ project_shared_path }}/uploads/'; // absolute path
 $config['allowed_types'] = '*'; //'gif|jpg|png|csv|txt|JPG|GIF|PNG|CSV|TXT';
 $config['max_size'] = '500000';
