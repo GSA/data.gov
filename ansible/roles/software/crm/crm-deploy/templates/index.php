60a1b639117df6246e8315f55e7cab131fd1284b
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-12-20 12:32:55 -0500

    log errors in prod

--- ansible/roles/software/crm/crm-deploy/templates/index.php
+++ ansible/roles/software/crm/crm-deploy/templates/index.php
@@ -34,11 +34,13 @@ if (defined('ENVIRONMENT'))
     {
         case 'development':
             error_reporting(E_ALL);
+            ini_set('display_errors', 1);
             break;
 
         case 'testing':
         case 'production':
-            error_reporting(0);
+            error_reporting(E_ALL & ~E_NOTICE);
+            ini_set('display_errors', 0);
             break;
 
         default:
