f1c9b6cade3f6a8d10646ce1645b4ad56ff7d434
aquilent-robproper <rob.proper@aquilent.com>
2017-01-19 13:58:53 -0500

    Reorganized the pipeline Jenkinsfile, as the load function requires a node
    #96

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,16 +1,81 @@
 #!groovy
 
 env.AWS_REGION = "us-east-2"
-def pipeline = null
+env.PIPELINE_SCRIPT = "./jenkins/full-pipeline.groovy"
 
 stage('Initialize') {
     node("master") {
         checkout scm
         sh "touch ~/ansible-secret.txt"
     }
-    pipeline = load("./jenkins/full-pipeline.groovy")
 }
 
-pipeline.run()
+
+// Assumes the script referenced in env.PIPELINE_SCRIPT
+// has functions named
+//   1. provision(environment) - that provisions the environment
+//   2. test() - that tests the provisioined servers, not used yet
+//   3. other, once implemented
+
+// Logic is to be added to select the correct pipeline script based 
+// on phrases inthe name of the job name (env.JOB_NAME)
+
+runPipeline()
 
 
+def runPipeline() {
+    runStages("dev")
+    runStages("prod")
+}
+
+
+def runStages(environment, isMaster) {
+    def isDev = (environment == "dev")
+    if (isDev(environment) || isMaster) {
+        provision(environment)
+        test(environment)
+        // do other stages here
+    } else {
+        // Do not process stages beyond dev, 
+        // unless master branch
+    }
+    
+}
+
+
+def provision(environment) {
+    stage("${labelEnvironment}: Provision") {
+        node('master') {
+            getPipeline().provision(nameEnvironment(environment))
+        }
+    }
+}
+
+def test(environment) {
+    stage("${labelEnvironment}: Test") {
+        node('master') {
+            getPipeline().test(nameEnvironment(environment))
+        }
+    }
+}
+
+
+def isMaster() {
+    return (env.BRANCH_NAME.startsWith("master"))
+}
+
+def isDev(environment) {
+    return  (environment == "dev")
+}
+
+def nameEnvironment(environment) {
+    return (isDev(environment) && !isMaster()) ? "-${env.BRANCH_NAME}" : ""
+}
+
+def labelEnvironment(environment) {
+    return environment.toUpperCase()
+}
+
+def getPipeline() {
+    return load(env.PIPELINE_SCRIPT)
+}
--- jenkins/full-pipeline.groovy
+++ jenkins/full-pipeline.groovy
@@ -1,46 +1,21 @@
 #!groovy
 
-def run() {
-    def isMaster = (env.BRANCH_NAME.startsWith("master"))
-    provision("dev", isMaster)
-    if (isMaster) {
-        // Add other environments here
-        provision("prod", isMaster)
-    }
-}
+def provision(environment) {
+    def terraform = load "./jenkins/terraform.groovy"
+    def playbook = load "./jenkins/playbook.groovy"
 
-def provision(environmentID, isMaster) {
-    def isDev = (environmentID == "dev")
-    def environment = environmentID + 
-        ((isDev && !isMaster) ? "-${env.BRANCH_NAME}" : "")
-    def label = environmentID.toUpperCase()
-
-    if (isDev || isMaster) {
-        return
-    }
+    terraform.run('infrastructure', environment)
     
-    stage("${label}: Provision") {
-        if (!isDev) {
-            timeout(time:5, unit:'DAYS') {
-                input "Proceed with deployment to ${label}?"
-            }
-        }
-
-        node("master") {
-            def terraform = load "./jenkins/terraform.groovy"
-            def playbook = load "./jenkins/playbook.groovy"
-
-            terraform.run('infrastructure', environment)
-            
-            terraform.run('pilot', environment, "infrastructure")
-            
-            playbook.run("jumpbox", "pilot", environment, 
-                "always,jumpbox,apache", "shibboleth", "bastion")
-            
-            playbook.run("datagov-web", "pilot", environment, null,
-                "trendmicro,vim,deploy,deploy-rollback,secops,postfix",
-                "wordpress-web", "wordpress-web")
-        }
-    }
+    terraform.run('pilot', environment, "infrastructure")
+    
+    playbook.run("jumpbox", "pilot", environment, 
+        "always,jumpbox,apache", "shibboleth", "bastion")
+    
+    playbook.run("datagov-web", "pilot", environment, null,
+        "trendmicro,vim,deploy,deploy-rollback,secops,postfix",
+        "wordpress-web", "wordpress-web")
 }
 
+def test(environment() {
+    // Nothing here yet
+}
