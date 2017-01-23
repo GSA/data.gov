7112751307218702fde0702c291707f710932145
rproper <rob.proper@aquilent.com>
2017-01-23 17:06:03 -0500

    Generate (environment specific) dynamic inventory file from pipeline
    #92

--- jenkins/provisioner/playbook.groovy
+++ jenkins/provisioner/playbook.groovy
@@ -6,8 +6,8 @@ def run(playbook, system, environment, resource, tags = null,
         def userName = "ubuntu"
         def credentialsID = getCredentialsId(system, environment, 
                 (resource != null) ? resource : playbook)
-        def hosts = getHosts(system, environment, resource)
-        def extras = "-vvvv --extra-vars \"playbook_hosts=${hosts} dummy=dummy\""
+        def inventoryName = newInventory(playbook, system, environment, resource)
+        def extras = "-i ${inventoryName} -vvvv"
 
         if (tags != null) {
             ansiblePlaybook playbook: "./${playbook}.yml",
@@ -32,6 +32,21 @@ def getCredentialsId(system, environment, resource) {
     def isDev = (environment.startsWith("dev-"))
     def securityContext = ((isDev) ? "dev" : environment)
     return "${env.AWS_REGION}-${system}_${securityContext}_${resource}"
+}
+
+def newInventory(playbook, system, environment, resource) {
+    def group_id = "${playbook}_hosts"
+    def host_tag = getHosts(system, environment, resource)
+    if (fileName == null) {
+        fileName = "./${playbook}.hosts"
+    }
+    sh "rm -f ${fileName}"
+    sh "echo '' > ${fileName}"
+    echo "group_id: ${group_id}; host_tag: ${host_tag}"
+    writeFile encoding: 'ascii', file: fileName, text: "[${group_id}]\n"
+    writeFile encoding: 'ascii', file: fileName, text: "${host_tag}\n"
+    sh "cat ${fileName}"
+    return fileName
 }
 
 def getHosts(system, environment, resource) {
