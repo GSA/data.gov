8d906b3b6ec365f73063bdf1d8cf258e19e559d8
rproper <rob.proper@aquilent.com>
2017-01-23 19:31:47 -0500

    Small change in pipeline
    #92

--- jenkins/provisioner/playbook.groovy
+++ jenkins/provisioner/playbook.groovy
@@ -6,11 +6,11 @@ def run(playbook, system, environment, resource, tags = null,
         def userName = "ubuntu"
         def credentialsID = getCredentialsId(system, environment, 
                 (resource != null) ? resource : playbook)
-        //def inventoryName = newInventory(playbook, system, environment, resource)
-        //def extras = "-i ${inventoryName} -vvvv"
-        def mapping = getDynamicMapping(system, environment, resource)
-        def variable = "${playbook}_hosts"
-        def extras = "--extra-vars \"${variable}=${mapping}\" -vvvv"
+        def inventoryName = newInventory(playbook, system, environment, resource)
+        def extras = "-i ${inventoryName} -vvvv"
+        // def mapping = getDynamicMapping(system, environment, resource)
+        // def variable = "${playbook}_hosts"
+        // def extras = "--extra-vars \"${variable}=${mapping}\" -vvvv"
 
         if (tags != null) {
             ansiblePlaybook playbook: "./${playbook}.yml",
@@ -37,19 +37,18 @@ def getCredentialsId(system, environment, resource) {
     return "${env.AWS_REGION}-${system}_${securityContext}_${resource}"
 }
 
-// def newInventory(playbook, system, environment, resource) {
-//     def group_id = "${playbook}_hosts"
-//     def variable = getDynamicMapping(system, environment, resource)
-//     def inventoryText = [ "[${group_id}]${variable}" ]
-//     def fileName = "./${playbook}.hosts"
-//     sh "rm -f ${fileName}"
-//     sh "echo '' > ${fileName}"
-//     echo "group_id: ${group_id}; variable: ${variable}"
-//     writeFile encoding: 'ascii', file: fileName, 
-//         text: "${inventoryText.join('\n')}\n"
-//     sh "cat ${fileName}"
-//     return fileName
-// }
+def newInventory(playbook, system, environment, resource) {
+    def group_id = "${playbook}_hosts"
+    def mapping = getDynamicMapping(system, environment, resource)
+    def inventoryText = "[${group_id}]\n${mapping}\n"
+    def fileName = "./${playbook}.hosts"
+    sh "rm -f ${fileName}"
+    sh "echo '' > ${fileName}"
+    echo "group_id: ${group_id}; variable: ${variable}"
+    writeFile encoding: 'ascii', file: fileName, text: inventoryText
+    sh "cat ${fileName}"
+    return fileName
+}
 
 def getDynamicMapping(system, environment, resource) {
     return "ec2_tag_Name_${system}_${environment}_${resource}"
