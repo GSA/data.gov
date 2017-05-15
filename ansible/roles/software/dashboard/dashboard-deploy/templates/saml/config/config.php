9641dcefbda801369ecd273b8cdc16d028ffb534
Alex Perfilov <alexandr.perfilov@reisystems.com>
2017-05-15 12:14:26 -0400

    Secure crm && dashboard SAML session cookies

--- ansible/roles/software/dashboard/dashboard-deploy/templates/saml/config/config.php
+++ ansible/roles/software/dashboard/dashboard-deploy/templates/saml/config/config.php
@@ -170,7 +170,7 @@ $config = array(
      * through https. If the user can access the service through
      * both http and https, this must be set to FALSE.
      */
-    'session.cookie.secure' => false,
+    'session.cookie.secure' => true,
 
     /*
      * Enable secure POST from HTTPS to HTTP.
