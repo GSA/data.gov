ff454ade0aedca6216fed7632b9f8b2935bfa4db
Aaron D Borden <adborden@a14n.net>
2018-10-03 10:11:35 -0700

    Add tests for native-login

--- ansible/roles/software/ckan/native-login/molecule/default/tests/test_default.py
+++ ansible/roles/software/ckan/native-login/molecule/default/tests/test_default.py
@@ -5,10 +5,29 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+virtualenv_path = '/usr/lib/ckan'
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_compatible_repoze_who(host):
+    packages = host.pip_package.get_packages(
+        pip_path=('%s/bin/pip' % virtualenv_path)
+    )
+
+    assert 'repoze.who' in packages
+    assert 'Paste' in packages
+
+    assert '2.0' == packages['repoze.who'].get('version')
+    assert '1.7.5.1' == packages['Paste'].get('version')
+
+
+def test_saml2_disabled(host):
+    production_ini = host.file('/etc/ckan/production.ini')
+
+    assert not production_ini.contains(r'^ckan.plugins\b.*\bsaml2\b'), \
+        'saml2 should not exist in ckan.plugins'
+
+
+def test_who_ini_config(host):
+    who_ini = host.file('/etc/ckan/who.ini')
+
+    assert not who_ini.contains(r'saml2')
