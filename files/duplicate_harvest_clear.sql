23ba54a7309d2461565fa085ff310a72b253a4ef
jbrown-xentity <jbrown@xentity.com>
2018-10-24 08:45:33 -0600

    Adding clarification and new harvest source

--- files/duplicate_harvest_clear.sql
+++ files/duplicate_harvest_clear.sql
@@ -1,8 +1,8 @@
 /* To clear 2 harvest sources at once, using DB queries.
    The harvest source ID's need to be found beforehand, you can search using the title or url like
-   "SELECT * FROM harvest_source WHERE title LIKE '%DOI%';"
-   These ID's need to be replaced in multiple places, noted by comments.
-   If this continues to fail, there are probably some datasets in the to_delete state that cannot be removed. Simple run
+   "SELECT * FROM harvest_source WHERE url LIKE '%data.doi.gov%' AND title LIKE '%DOI%' AND url NOT LIKE '%WAF%';"
+   These ID's need to be added/replaced in multiple places, noted by comments.
+   If this continues to fail, there are probably some datasets in the to_delete state that cannot be removed. Simply run
        "UPDATE package SET state = 'to_remove' WHERE state = 'to_delete'"
    Then run the sql below, then reset the database to where it was before:
        "UPDATE package SET state = 'to_delete' WHERE state = 'to_remove'"
@@ -10,21 +10,45 @@
 
 begin;
     /* Replace ID's below */
-    update package set state = 'to_delete' where id in (select package_id from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7');
-    delete from resource_view where resource_id in (select id from resource where package_id in (select id from package where state = 'to_delete' ));
+    update package set state = 'to_delete' where id in (
+      select package_id from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' 
+      OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7' 
+      OR harvest_source_id = 'd8ef5c4f-b0fe-4338-afe9-089c029ea378'
+    );
+    delete from resource_view where resource_id in (
+      select id from resource where package_id in (
+        select id from package where state = 'to_delete' 
+      )
+    );
     delete from resource_revision where package_id in (select id from package where state = 'to_delete' );
     delete from resource where package_id in (select id from package where state = 'to_delete' );
     
     /* Replace ID's below */
-    delete from harvest_object_error where harvest_object_id in (select id from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7');
+    delete from harvest_object_error where harvest_object_id in (
+      select id from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' 
+      OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7'
+      OR harvest_source_id = 'd8ef5c4f-b0fe-4338-afe9-089c029ea378'
+    );
     /* Replace ID's below */
-    delete from harvest_object_extra where harvest_object_id in (select id from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7');
+    delete from harvest_object_extra where harvest_object_id in (
+      select id from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' 
+      OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7'
+      OR harvest_source_id = 'd8ef5c4f-b0fe-4338-afe9-089c029ea378'
+    );
     /* Replace ID's below */
-    delete from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7';
+    delete from harvest_object where harvest_source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' 
+    OR harvest_source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7'
+    OR harvest_source_id = 'd8ef5c4f-b0fe-4338-afe9-089c029ea378';
     /* Replace ID's below */
-    delete from harvest_gather_error where harvest_job_id in (select id from harvest_job where source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' OR source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7');
+    delete from harvest_gather_error where harvest_job_id in (
+      select id from harvest_job where source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' 
+      OR source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7'
+      OR source_id = 'd8ef5c4f-b0fe-4338-afe9-089c029ea378'
+    );
     /* Replace ID's below */
-    delete from harvest_job where source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' OR source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7';
+    delete from harvest_job where source_id = '34ce571b-cb98-4e0b-979f-30f9ecc452c5' 
+    OR source_id = '1f3ebea5-58d2-4aaa-85a0-e7ebda3074e7'
+    OR source_id = 'd8ef5c4f-b0fe-4338-afe9-089c029ea378';
     delete from package_role where package_id in (select id from package where state = 'to_delete' );
 
     DELETE FROM user_object_role
