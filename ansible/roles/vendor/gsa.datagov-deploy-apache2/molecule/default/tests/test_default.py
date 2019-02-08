98c342f8eb69fa5b099191af76c75efcb63fbcdc
Aaron D Borden <adborden@a14n.net>
2019-02-07 16:04:30 -0800

    Bump apache2 role for TLS/SSL configuration

--- ansible/roles/vendor/gsa.datagov-deploy-apache2/molecule/default/tests/test_default.py
+++ ansible/roles/vendor/gsa.datagov-deploy-apache2/molecule/default/tests/test_default.py
@@ -5,7 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-python_prefix = '/usr/local'
+python_prefix = '/usr'
 
 
 def test_wsgi_python_home(host):
@@ -26,10 +26,31 @@ def test_apache2(host):
     assert service.is_running
 
 
-def test_ports(host):
+def test_http(host):
     http = host.socket('tcp://80')
 
     assert http.is_listening
+
+
+def test_https(host):
+    https = host.socket('tcp://443')
+
+    assert https.is_listening
+
+
+def test_www(host):
+    uri = host.ansible('uri', 'url="http://localhost/"', check=False)
+
+    assert uri['status'] == 200
+
+
+def test_no_indexes(host):
+    # Test we don't get an Indexes/MultiView from http://localhost:443
+    # TODO we should set up the role to assume redirect http -> https which
+    # would remove the need for this.
+    uri = host.ansible('uri', 'url="http://localhost:443/?M=A"', check=False)
+
+    assert uri['status'] == 403
 
 
 def test_ssl_versions(host):
