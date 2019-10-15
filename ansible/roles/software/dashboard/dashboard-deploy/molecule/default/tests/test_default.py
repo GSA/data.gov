345ba38b8aef089744a0f50294da90350c8d4681
Aaron D Borden <adborden@a14n.net>
2019-10-15 16:58:25 -0700

    [dashboard] use supervisor for cron jobs
    Use supervisor to handle job control (avoids having multiple jobs run at once)
    and logging. Create a log directory for Dashboard related log files. Adds
    logrotate to manage these files and email the logs out daily.

--- ansible/roles/software/dashboard/dashboard-deploy/molecule/default/tests/test_default.py
+++ ansible/roles/software/dashboard/dashboard-deploy/molecule/default/tests/test_default.py
@@ -6,7 +6,36 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    hosts = host.file('/etc/hosts')
+dashboard_user = 'ubuntu'
 
-    assert hosts.exists
+
+def test_log_dir(host):
+    log_dir = host.file('/var/log/dashboard')
+
+    assert log_dir.exists
+    assert log_dir.is_directory
+    assert log_dir.user == dashboard_user
+    assert log_dir.group == dashboard_user
+
+
+def test_supervisor_conf(host):
+    supervisor = host.file('/etc/supervisor/conf.d/dashboard')
+
+    assert supervisor.exists
+    assert supervisor.user == 'root'
+    assert supervisor.group == 'root'
+    assert supervisor.mode == 0o644
+    assert supervisor.contains('program:dashboard-cfo-act-download')
+    assert supervisor.contains('program:dashboard-cfo-act-full-scan')
+    assert supervisor.contains('user=%s' % dashboard_user)
+
+
+def test_cron(host):
+    cron = host.file('/etc/cron.d/dashboard')
+
+    assert cron.exists
+    assert cron.user == 'root'
+    assert cron.group == 'root'
+    assert cron.mode == 0o644
+    assert cron.contains('supervisorctl start dashboard-cfo-act-download')
+    assert cron.contains('supervisorctl start dashboard-cfo-act-full-scan')
