c588626f25a6a6a900ced75730699a7e7b63f0fb
Aaron D Borden <adborden@a14n.net>
2019-10-15 09:32:51 -0700

    [dashboard] add molecule

---
+++ ansible/roles/software/dashboard/dashboard-deploy/molecule/default/tests/test_default.py
@@ -0,0 +1,12 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    hosts = host.file('/etc/hosts')
+
+    assert hosts.exists
