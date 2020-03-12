4735526f1facd695046dde88a1ceffce89c629b3
Aaron D Borden <adborden@a14n.net>
2020-03-12 16:49:48 -0700

    [catalog] move who.ini configuration into ckan-app

--- ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
+++ ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
@@ -40,9 +40,28 @@ def test_dynamic_menu(host):
     assert dynamic_menu.mtime < datetime(2020, 1, 2)
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_production_ini(host):
+    production_ini = host.file('/etc/ckan/production.ini')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert production_ini.exists
+    assert production_ini.user == 'root'
+    assert production_ini.group == 'www-data'
+    assert production_ini.mode == 0o640
+
+    assert production_ini.contains('ckan.plugins =.*datajson')
+    assert not production_ini.contains('ckan.plugins =.*saml2')
+
+
+def test_who_ini(host):
+    who_ini = host.file('/etc/ckan/who.ini')
+
+    assert who_ini.exists
+    assert who_ini.user == 'root'
+    assert who_ini.group == 'www-data'
+    assert who_ini.mode == 0o640
+
+    assert who_ini.contains(
+        '^use = repoze.who.plugins.friendlyform:FriendlyFormPlugin'
+    )
+
+    assert not who_ini.contains('saml2auth')
