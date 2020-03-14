9dd8f8e5459af1d39123fdaf15d43f2d93b9fcdd
Aaron D Borden <adborden@a14n.net>
2020-03-13 17:23:08 -0700

    [catalog] create CKAN tmp/lock directory

--- ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
+++ ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
@@ -6,6 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_var_tmp_ckan(host):
+    var_tmp_ckan = host.file('/var/tmp/ckan')
+
+    assert var_tmp_ckan.exists
+    assert var_tmp_ckan.is_directory
+    assert var_tmp_ckan.user == 'www-data'
+    assert var_tmp_ckan.group == 'www-data'
+    assert var_tmp_ckan.mode == 0o755
+
+
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
