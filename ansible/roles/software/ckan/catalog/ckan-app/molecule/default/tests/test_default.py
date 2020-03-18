0f9853db7af338b7b1c2690e041dbd31f53b9861
Aaron D Borden <adborden@a14n.net>
2020-03-18 09:48:33 -0700

    [catalog] create CKAN storage dir

--- ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
+++ ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
@@ -6,6 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_var_lib_ckan(host):
+    var_lib_ckan = host.file('/var/lib/ckan')
+
+    assert var_lib_ckan.exists
+    assert var_lib_ckan.is_directory
+    assert var_lib_ckan.user == 'www-data'
+    assert var_lib_ckan.group == 'www-data'
+    assert var_lib_ckan.mode == 0o755
+
+
 def test_var_tmp_ckan(host):
     var_tmp_ckan = host.file('/var/tmp/ckan')
 
