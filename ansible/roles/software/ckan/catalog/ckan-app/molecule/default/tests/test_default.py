84486033bf6141a2cd44d76668a73e246e7dd4f2
Aaron D Borden <adborden@a14n.net>
2020-03-20 15:15:21 -0700

    [catalog] set menu.json as stale
    Set menu.json with an old modification time so that ckanext-datagovtheme will
    attempt to update it on startup.

--- ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
+++ ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import testinfra.utils.ansible_runner
 
@@ -24,6 +25,19 @@ def test_var_tmp_ckan(host):
     assert var_tmp_ckan.user == 'www-data'
     assert var_tmp_ckan.group == 'www-data'
     assert var_tmp_ckan.mode == 0o755
+
+
+def test_dynamic_menu(host):
+    """Test menu.json is prepopulated with old modification time
+    https://github.com/GSA/catalog-app/issues/76"""
+    dynamic_menu = host.file('/var/tmp/ckan/dynamic_menu/menu.json')
+
+    assert dynamic_menu.exists
+    assert dynamic_menu.user == 'www-data'
+    assert dynamic_menu.group == 'www-data'
+    assert dynamic_menu.mode == 0o644
+    # We do a loose assertion on mtime to avoid timezone issues
+    assert dynamic_menu.mtime < datetime(2020, 1, 2)
 
 
 def test_hosts_file(host):
