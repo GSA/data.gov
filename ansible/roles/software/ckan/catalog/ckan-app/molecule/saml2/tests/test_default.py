966e9c2fdc640756bef6e96794df592fad9b6413
Fuhu Xia <fxia@reisystems.com>
2020-05-08 12:25:20 -0400

    remove datajson from catalog plugins

--- ansible/roles/software/ckan/catalog/ckan-app/molecule/saml2/tests/test_default.py
+++ ansible/roles/software/ckan/catalog/ckan-app/molecule/saml2/tests/test_default.py
@@ -15,7 +15,7 @@ def test_production_ini(host):
     assert production_ini.group == 'www-data'
     assert production_ini.mode == 0o640
 
-    assert production_ini.contains('ckan.plugins =.*datajson')
+    assert production_ini.contains('ckan.plugins =.*datajson_harvest')
     assert production_ini.contains('ckan.plugins =.*saml2')
 
 
