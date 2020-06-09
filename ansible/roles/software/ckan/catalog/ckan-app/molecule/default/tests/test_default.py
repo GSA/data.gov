def5402af1ba6bc1469465ebdc1a1c9ccf45d8c6
Fuhu Xia <fxia@reisystems.com>
2020-06-09 12:18:12 -0400

    add test for mem-cpu-check

--- ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
+++ ansible/roles/software/ckan/catalog/ckan-app/molecule/default/tests/test_default.py
@@ -113,3 +113,20 @@ def test_apache_site(host):
         'Expected no rewrite rule for login URLs'
     assert not f.contains('RewriteCond.*!auth_tkt'), \
         'Expected no rewrite condition for unauthenticated requests'
+
+def test_web_mem_cpu_conf(host):
+    f = host.file('/etc/supervisor/conf.d/supervisord_web_mem_cpu.conf')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+    assert f.contains('[program:check-mem-cpu]')
+
+def test_cpu_mem_check_sh(host):
+    f = host.file('/usr/bin/cpu-mem-check.sh')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o755
