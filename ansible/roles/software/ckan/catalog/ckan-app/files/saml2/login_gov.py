57712f2785316ea827f50e466c2ab8278372cdb2
Tom Wood <tom.wood@civicactions.com>
2020-11-13 14:22:41 -0500

    Add attribute map specifically for login.gov

---
+++ ansible/roles/software/ckan/catalog/ckan-app/files/saml2/login_gov.py
@@ -0,0 +1,8 @@
+MAP = {
+    "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+    "fro": {
+        'uuid': 'uuid',
+        'email': 'email'
+    }
+    # "to" mapping should be created automatically
+}
