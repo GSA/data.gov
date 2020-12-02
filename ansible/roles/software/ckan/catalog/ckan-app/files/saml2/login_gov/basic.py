dee8bdfb961126de3af81012e561b5004c661f14
Tom Wood <tom.wood@civicactions.com>
2020-12-02 11:48:30 -0500

    Need both "to" and "fro" maps, apparently!

--- ansible/roles/software/ckan/catalog/ckan-app/files/saml2/login_gov/basic.py
+++ ansible/roles/software/ckan/catalog/ckan-app/files/saml2/login_gov/basic.py
@@ -1,6 +1,10 @@
 
 MAP = {
     "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+    "to": {
+        'uuid': 'uuid',
+        'email': 'email'
+    },
     "fro": {
         'uuid': 'uuid',
         'email': 'email'
