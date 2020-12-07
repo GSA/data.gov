4e2e05ccaa27737d7edeb2b59599ea70c006c627
jbrown-xentity <jbrown@xentity.com>
2020-12-07 16:17:46 -0700

    re-org login.gov integration

---
+++ ansible/roles/software/ckan/saml2/files/etc/ckan/saml2/login.gov/basic.py
@@ -0,0 +1,12 @@
+
+MAP = {
+    "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+    "to": {
+        'uuid': 'uuid',
+        'email': 'email'
+    },
+    "fro": {
+        'uuid': 'uuid',
+        'email': 'email'
+    }
+}
