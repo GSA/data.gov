d4064bdc1759ceda6d539c7f309814e6e8c9add1
robert-bryson <robert.bryson@gsa.gov>
2022-09-27 11:05:34 -0700

    shellcheck seems to prefer this syntax

--- bin/ckan/org_dataset_count.sh
+++ bin/ckan/org_dataset_count.sh
@@ -8,7 +8,7 @@ curl -sL $route_to_check/api/action/organization_list | jq -rc .result[] | while
 do
   dataset_count=$(curl -sL "$route_to_check/api/action/package_search?fq=organization:$org" | jq .result.count)
   if [[ "$dataset_count" = "0" ]]; then
-    organization_with_0_datasets=$(($organization_with_0_datasets+1))
+    organization_with_0_datasets=$(( organization_with_0_datasets + 1 ))
   fi
   echo "$org: $dataset_count"
   echo $organization_with_0_datasets with 0 datasets
