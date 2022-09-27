dab25b08fb867527ab1b8d87dfeb0e50786f548c
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-27 13:51:03 -0400

    new: query for 0 dataset organizations

---
+++ bin/ckan/org_dataset_count.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+route_to_check='https://catalog-prod-admin-datagov.app.cloud.gov'
+
+organization_with_0_datasets=0
+
+curl -sL $route_to_check/api/action/organization_list | jq -rc .result[] | while read -r org
+do
+  dataset_count=$(curl -sL "$route_to_check/api/action/package_search?fq=organization:$org" | jq .result.count)
+  echo "$org: $dataset_count"
+  if [[ "$dataset_count" = "0" ]]; then
+    organization_with_0_datasets=$((organization_with_0_datasets+1))
+  fi
+done
+
+echo $organization_with_0_datasets with 0 datasets
