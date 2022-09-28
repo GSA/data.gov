183a46e420c37ef6a59427e2bcb12b29a7bc03a0
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-28 17:06:06 -0400

    new: duplicate identifier checker
    Add new helper script that finds datasets with duplicate identifiers

---
+++ bin/ckan/org_duplicate_3968.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "" > output
+
+while read -r data_identifier
+do
+  duplicates=$(curl -sL "https://catalog.data.gov/api/action/package_search?q=identifier:%22$data_identifier%22")
+  organization=$(echo $duplicates | jq -r '.result.results[0].organization.name')
+  dataset_id=$(echo $duplicates | jq .result.results[].id)
+  echo "$data_identifier: $organization: $dataset_id" >> output
+done <<< $(curl -sL 'https://catalog.data.gov/api/action/package_search?fq=harvest_source_id:*&facet.field=[%22identifier%22]&facet.limit=-1&facet.mincount=2' | jq -r '.result.facets.identifier' | jq -r keys | jq -r .[])
